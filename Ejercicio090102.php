cd79c938c8a7720bc8e4755b01fe1bf82d1498c3
Andres Ramos <58173665+argavellaneda@users.noreply.github.com>
2019-12-17 21:29:46 +0100

    Create Ejercicio090102.php
    Ejercicio090102.php de prueba

---
+++ Ejercicio090102.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+  <title>Ejemplos Varios</title>
+  </head>
+  <body>
+  <h1 align="center">Salida del fichero : <u> <? echo __FILE__ ?> </u></h1>
+  <hr>
+  <?php
+  echo("esto es una prueba de PHP y git");
+  ?>
+  <? $cliente = "Pepito Perez"; echo "El nombre del cliente es $cliente" . " y nació en Madrid"; ?>
+  </body>
+</html>
